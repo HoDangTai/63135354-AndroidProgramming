5eeb318b7ca765a9692cba28abf97462788609ad
HoDangTai <152425828+HoDangTai@users.noreply.github.com>
2024-03-24 20:24:31 +0700

    KiemTraGiuaKi

--- ntu_63135354/Cau3_AppGioiThieu/app/src/main/java/ntu/tai_63135354/cau3_appgioithieu/MainActivity.java
+++ ntu_63135354/Cau3_AppGioiThieu/app/src/main/java/ntu/tai_63135354/cau3_appgioithieu/MainActivity.java
@@ -16,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView img = findViewById(R.id.imageView);
 
-
+        img.setImageResource(R.mipmap.hotai4x6);
 
 
 
